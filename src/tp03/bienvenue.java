5cef88fb569a2d9f6a4effab82d8ce4e4f5aaee8
formation_gep <chalendard.remy@gmail.com>
2020-01-15 09:03:39 +0100

    commit version 1.0

---
+++ src/tp03/bienvenue.java
@@ -0,0 +1,18 @@
+package tp03;
+public class bienvenue {
+public static void main(String[] args) {
+
+    afficheBanniere();
+//  suite du programme
+}
+public static void afficheBanniere() {
+
+
+System.out.println("*********************************************************");
+System.out.println("*****                                               *****");
+System.out.println("*****                   Bienveue                    *****");
+System.out.println("*****                                               *****");
+System.out.println("*****                                               *****");
+System.out.println("*********************************************************");
+    }  
+}
